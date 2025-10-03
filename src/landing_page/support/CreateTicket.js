706e8a44c8192e97e0ff111aae16b235cc0e3424
Vaibhav-Pacherwal <vaibhavpacherwal57@gmail.com>
2025-10-03 17:44:39 +0000

    support

---
+++ src/landing_page/support/CreateTicket.js
@@ -0,0 +1,65 @@
+export default function CreateTicket() {
+    return (
+        <div className="container mt-5 mb-5">
+            <div className="row mb-4 mt-5">
+                <h1>
+                    To create a ticket, select a relevant topic
+                </h1>
+            </div>
+            <div className="row mb-3 p-5">
+                <div className="col-4 mb-5">
+                    <p className="fs-3 mb-5"><i class="fa-solid fa-square-plus"></i> Account</p>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Resident individual</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Minor</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Non Resident Indian (NRI)</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Company, Partnership, HUF and LLP</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Glossary</a>
+                </div>
+                <div className="col-4 mb-5">
+                    <p className="fs-3 mb-5"><i class="fa-solid fa-square-poll-vertical"></i> Kite</p>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>IPO</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Trading FAQs</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Margin Trading Facility (MTF) and Margins</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Charts and orders</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Alerts and Nudges</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>General</a>
+                </div>
+                <div className="col-4 mb-5">
+                    <p className="fs-3 mb-5"><i class="fa-solid fa-user"></i> Your Zerodha Account</p>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Your Profile</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Account modification</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Client Master Report (CMR) and Depository Participant (DP)</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Nomination</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Transfer and conversion of securities</a>
+                </div>
+            </div>
+            <div className="row mb-5 p-5">
+                <div className="col-4">
+                    <p className="fs-3 mb-5"><i class="fa-solid fa-wallet"></i> Funds</p>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Add Money</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Withdrawl Money</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Add Bank Accounts</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Add eMandates</a>
+                </div>
+                <div className="col-4">
+                    <p className="fs-3 mb-5"><i class="fa-solid fa-circle-notch"></i> Console</p>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Portfolio</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Corporate actions</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Funds statement</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Reports</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Profile</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Segments</a>
+                </div>
+                <div className="col-4">
+                    <p className="fs-3 mb-5"><i class="fa-regular fa-circle"></i> Coin</p>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Mutual funds</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>National Pension Scheme (NPS)</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Fixed Deposit (FD)</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Features on Coin</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>Payments and Orders</a>
+                    <a href="#" className="d-block mb-3 fs-5" style={{textDecoration: "none"}}>General</a>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/landing_page/support/SupportPage.js
+++ src/landing_page/support/SupportPage.js
@@ -1,5 +1,11 @@
+import Hero from "./Hero"
+import CreateTicket from "./CreateTicket"
+
 export default function SupportPage() {
     return (
-        <h1>SupportPage</h1>
+        <>
+          <Hero />
+          <CreateTicket />
+        </>
     )
 }
